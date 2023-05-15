288268c94fb80c9a39ed460b1d43dfd0b1c43923
HridoyHoque <hhridoy155@gmail.com>
2023-05-15 18:08:08 +0600

    Added Banner and About us section

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
+import About from '../../About/About';
 
 const Home = () => {
     return (
         <div>
-           <h2>This is home page</h2> 
+            <Banner></Banner>
+            <About></About>
         </div>
     );
 };

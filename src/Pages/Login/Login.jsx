f4c4291187b9b61315ec0b51ab09c38217573334
HridoyHoque <hhridoy155@gmail.com>
2023-05-17 10:35:23 +0600

    implemented private routes

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom';
 import img from '../../assets/images/login/login.svg'
 import { useContext } from 'react';
 import { AuthContext } from '../../Providers/AuthProviders';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Login = () => {
     const { signIn } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+    const from = location.state?.from?.pathname || "/";
 
     const handleLogin = (event) => {
         event.preventDefault()
@@ -18,6 +22,7 @@ const Login = () => {
             .then(result => {
                 const user = result.user;
                 console.log(user);
+                navigate(from, {replace: true});
             })
             .catch(error => console.log(error))
         form.reset()
